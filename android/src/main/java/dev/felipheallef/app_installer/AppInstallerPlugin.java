c8a756c7e5d65da01c50597d2a6b4b22c9f772d6
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2023-04-03 15:06:03 +0530

    default is failiure

--- android/src/main/java/dev/felipheallef/app_installer/AppInstallerPlugin.java
+++ android/src/main/java/dev/felipheallef/app_installer/AppInstallerPlugin.java
@@ -400,6 +400,8 @@ public class AppInstallerPlugin implements FlutterPlugin, ActivityAware,
                 }
                     break;
                 default:
+                    methodChannel.invokeMethod("installer.faliure", packageName);
+
             }
         }
 
